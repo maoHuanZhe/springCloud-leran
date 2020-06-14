bb6052ef196597fffc2a2efa8393412739a44f32
fanguangrui <898365387@qq.com>
2020-06-14 14:01:19 +0800

    搭建EurekaServer集群环境

---
+++ cloud-eureka-server7002/src/main/java/com/fgrapp/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.fgrapp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaMain7002
+ *
+ * @author fan guang rui
+ * @date 2020年06月14日 13:33
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
