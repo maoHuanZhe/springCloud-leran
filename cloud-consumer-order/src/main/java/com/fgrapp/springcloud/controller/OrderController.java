43f93facec84afeef62accd325ac40219372176f
fanguangrui <898365387@qq.com>
2020-06-14 16:31:10 +0800

    服务发现Discover

--- cloud-consumer-order/src/main/java/com/fgrapp/springcloud/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/com/fgrapp/springcloud/controller/OrderController.java
@@ -3,12 +3,15 @@ package com.fgrapp.springcloud.controller;
 import com.fgrapp.springcloud.entities.CommonResult;
 import com.fgrapp.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * OrderController
@@ -23,7 +26,9 @@ public class OrderController {
 //    public static final String PAYMENT_URL = "http://localhost:8001"; //单机版
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE"; //集群版
     @Resource
-    public RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
@@ -33,5 +38,24 @@ public class OrderController {
     @GetMapping("consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id")Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("consumer/payment/discovery")
+    public CommonResult getPaymentDiscovery(){
+        return restTemplate.getForObject(PAYMENT_URL+"/payment/discovery",CommonResult.class);
+    }
+
+    @GetMapping("/consumer/discovery")
+    public CommonResult discovery(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(element->{
+            log.info("*****element:{}",element);
+        });
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-ORDER-SERVICE");
+        instances.forEach(serviceInstance -> {
+            log.info(serviceInstance.getServiceId()+"\t"+serviceInstance.getHost()+"\t"+serviceInstance.getPort()+"\t"+serviceInstance.getUri());
+        });
+        return new CommonResult(200,"查询成功",discoveryClient);
     }
 }
--- cloud-provider-payment/src/main/java/com/fgrapp/springcloud/controller/PaymentController.java
+++ cloud-provider-payment/src/main/java/com/fgrapp/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.fgrapp.springcloud.entities.Payment;
 import com.fgrapp.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  *
@@ -21,6 +24,8 @@ public class PaymentController {
     private PaymentService paymentService;
     @Value("${server.port}")
     private  int port;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -45,5 +50,19 @@ public class PaymentController {
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public CommonResult discovery(){
+        List<String> services = discoveryClient.getServices();
+        services.forEach(element->{
+            log.info("*****element:{}",element);
+        });
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(serviceInstance -> {
+            log.info(serviceInstance.getServiceId()+"\t"+serviceInstance.getHost()+"\t"+serviceInstance.getPort()+"\t"+serviceInstance.getUri());
+        });
+        return new CommonResult(200,"查询成功",discoveryClient);
     }
 }
