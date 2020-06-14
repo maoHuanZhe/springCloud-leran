0d3bfbb1d1a40c60143f6110b5ce7bd635eb00ac
fanguangrui <898365387@qq.com>
2020-06-14 13:01:53 +0800

    创建Eureka服务注册中心

---
+++ cloud-eureka-server/src/main/java/com/fgrapp/springcloud/EurekaMain.java
@@ -0,0 +1,19 @@
+package com.fgrapp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EurekaMain
+ *
+ * @author fan guang rui
+ * @date 2020年06月14日 11:19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain.class,args);
+    }
+}
