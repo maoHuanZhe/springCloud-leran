ecea70f5f58ba151c1060371ac38e84a8086d1ff
fanguangrui <898365387@qq.com>
2020-06-14 13:04:12 +0800

    修改 payment为 服务提供者 consumer为服务消费者

--- cloud-consumer-order/src/main/java/com/fgrapp/springcloud/OrderMain.java
+++ cloud-consumer-order/src/main/java/com/fgrapp/springcloud/OrderMain.java
@@ -2,6 +2,7 @@ package com.fgrapp.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020年06月13日 20:16
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
